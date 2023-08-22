cb6c18ab1f52234ad1f57a259a2c691c0736ee63
Ankit <ankitsharmagh093@gmail.com>
2023-08-22 17:33:44 +0530

    posts fetched in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
-"use client";
+// "use client";
 import Image from "next/image";
 import noTask from "/public/no-tasks.png";
 
-export default function Page() {
-  const data = [];
+export default async function Page() {
+  const getPosts = async () => {
+    try {
+      const data = await fetch("http://localhost:3000/posts");
+      const posts = await data.json();
+      return posts;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  type toDo =
+    | Array<{
+        _id: String;
+        topic: String;
+        description: String;
+        tags: Array<String>;
+      }>
+    | [];
+  const data: toDo = await getPosts();
+
   return (
     <div className="p-8">
       <h1 className="text-3xl text-center">Welcome to To-Do app</h1>
       <div>
         {data.length ? (
-          <h2>Some Tasks</h2>
+          data.map((item, key) => (
+            <div key={key}>
+              <p>{item.topic}</p>
+              <p>{item.description}</p>
+              <p>{item.tags}</p>
+            </div>
+          ))
         ) : (
           <div className="m-auto mt-8 lg:p-8  sm:[width:600px] ">
             <div className="m-auto ">
